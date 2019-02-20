2f1427b6248b646c6d39e17684df82e7ccbc6f55
mohberliannusantara <liannusantara1158@gmail.com>
2019-02-20 11:43:50 +0700

    manage view for users and admin

--- application/controllers/Autentikasi.php
+++ application/controllers/Autentikasi.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Welcome extends CI_Controller {
+class Autentikasi extends CI_Controller {
 
 	/**
 	 * Index Page for this controller.
@@ -21,7 +21,7 @@ class Welcome extends CI_Controller {
 	public function index()
 	{
 		$this->load->view('pages-template/header');
-		$this->load->view('welcome_message');
+		$this->load->view('autentikasi/index');
 		$this->load->view('pages-template/footer');
 	}
 }
---
+++ application/views/pages-template/footer.php
@@ -0,0 +1,26 @@
+<!-- ============================================================== -->
+<!-- All Jquery -->
+<!-- ============================================================== -->
+<script src="<?php echo base_url('assets/plugins/jquery/jquery.min.js') ?>"></script>
+<!-- Bootstrap tether Core JavaScript -->
+<script src="<?php echo base_url('assets/plugins/bootstrap/js/popper.min.js') ?>"></script>
+
+<script src="<?php echo base_url('assets/plugins/bootstrap/js/bootstrap.min.js') ?>"></script>
+<!--Wave Effects -->
+<script src="<?php echo base_url('assets/js/waves.js') ?>"></script>
+<!-- slimscrollbar scrollbar JavaScript -->
+<script src="<?php echo base_url('assets/js/jquery.slimscroll.js') ?>"></script>
+<!--Menu sidebar -->
+<script src="<?php echo base_url('assets/js/sidebarmenu.js') ?>"></script>
+<!--stickey kit -->
+<script src="<?php echo base_url('assets/plugins/sticky-kit-master/dist/sticky-kit.min.js') ?>"></script>
+<script src="<?php echo base_url('assets/plugins/sparkline/jquery.sparkline.min.js') ?>"></script>
+<!--Custom JavaScript -->
+<script src="<?php echo base_url('assets/js/custom.min.js') ?>"></script>
+<!-- ============================================================== -->
+<!-- Style switcher -->
+<!-- ============================================================== -->
+<script src="<?php echo base_url('assets/plugins/styleswitcher/jQuery.style.switcher.js') ?>"></script>
+</body>
+
+</html>
